548187c5cc050a874ac2b37af7fb3097f96bda16
Roman Pinelo <prof.roman.pinelo@gmail.com>
2021-02-05 13:38:23 -0600

    Página terminada

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# area_perimetro
+# Área y perímetro 
+
+Tomando como base la práctica de "Dibujando en canvas", se piden los datos de la base y altura de un rectángulo y se calculan el área y perímetro. A su vez, se dibuja el rectángulo en un canvas.
+
+
+## Construido con 🛠️
+
+* HTML
+* CSS
+* JS
+
+
+## Autores ✒️
+
+José Román Pinelo Escobar
+
+
+### Explicación
+
+Esta es la pantalla principal donde, en dos input se pide el valor de la base y alrura de un rectángulo:
+
+![1](https://user-images.githubusercontent.com/71656431/107079933-2e188600-67b6-11eb-940a-aa5efb750fbc.jpg)
+
+
+Al dar click en el botón 'Calcular', aparecerá el resultado en la pantalla y se dibujará un rectángulo en el canvas. Al canvas se le agregó un efecto de sombra que cambia de color:
+
+![2](https://user-images.githubusercontent.com/71656431/107079951-3375d080-67b6-11eb-8111-26245238f3d2.jpg)
+
+
+
+También se agregó un botón para borrar los resultados, al presionarlo se borra tanto el resultado como el dibujo.
+
+![3](https://user-images.githubusercontent.com/71656431/107079966-3a9cde80-67b6-11eb-88c1-bd910375bcd0.jpg)
+
+
+
+En el siguiente link puedes ver la página funcionando: 
+
+https://romanpinelo.github.io/
---
+++ calculo.js
@@ -0,0 +1,149 @@
+/**********************************************************************
+
+    TOMAR LOS DATOS DE LOS INPUT Y CALCULAR ÁREA Y PERÍMETRO
+
+**********************************************************************/
+/* Datos de altura y base */
+var base = document.getElementById("longitudBase");
+var altura = document.getElementById("longitudAltura");
+
+
+function calcularAreaPerimetro() {
+  var longitudBase = parseInt(base.value);
+  var longitudAltura = parseInt(altura.value);
+  
+  var area,perimetro;
+  
+  area = longitudBase * longitudAltura; 
+  perimetro = (longitudBase * 2) + (longitudAltura * 2);
+  
+  document.getElementById('resultadoArea').innerHTML=area + " unidades cuadradas";
+  document.getElementById('resultadoPerimetro').innerHTML=perimetro + " unidades";
+  
+  dibujarRectangulo(longitudBase, longitudAltura);
+
+}
+
+/* Botón de calcular */
+var boton = document.getElementById("botonCalcular");
+boton.addEventListener("click", calcularAreaPerimetro );
+
+
+/*************************************************************************************
+
+    BORRAR EL CONTENIDO DE LOS INPUT AL VOLVER A HACER CLICK EN ELLOS
+
+************************************************************************************/
+//Borrar contenido del input de la base. 
+var borrar_input_base = document.getElementById("longitudBase");
+borrar_input_base.addEventListener("click", borraInputBase);
+
+function borraInputBase ()
+{
+  borrar_input_base = this.value="";
+}
+
+//Borrar contenido del input de la altura. 
+var borrar_input_altura = document.getElementById("longitudAltura");
+borrar_input_altura.addEventListener("click", borraInputAltura);
+
+function borraInputAltura ()
+{
+  borrar_input_altura = this.value="";
+}
+
+/**********************************************************************************
+
+    DIBUJAR EN EL CANVAS
+
+**********************************************************************************/
+/* Datos del canvas */
+var d = document.getElementById("dibujoRectangulo");
+var ancho = d.width;
+var alto = d.height;
+var lienzo = d.getContext("2d");
+
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.lineWidth = 3;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+function dibujarRectangulo (Base, Altura) {
+  var xi = (ancho / 2) - (Base / 2);
+  var yi = (alto / 2) - (Altura / 2);
+  var xf = xi + Base;
+  var yf = yi;
+  dibujarLinea("red", xi, yi, xf, yf);
+  
+  xi = xf;
+  yi = yf;
+  xf = xi;
+  yf = yi + Altura;
+  dibujarLinea("red", xi, yi, xf, yf);
+  
+  xi = xf;
+  yi = yf;
+  xf = xi - Base;
+  yf = yi;
+  dibujarLinea("red", xi, yi, xf, yf);
+  
+  xi = xf;
+  yi = yf;
+  xf = xi;
+  yf = yi - Altura;
+  dibujarLinea("red", xi, yi, xf, yf);
+}
+
+
+
+/**********************************************************************************
+
+    DIBUJAR EN EL CANVAS (ALTERNATIVA DE RECTÁNGULO RELLENO)
+
+**********************************************************************************/
+/* Datos del canvas */
+/*var d = document.getElementById("dibujoRectangulo");
+var ancho = d.width;
+var alto = d.height;
+var lienzo = d.getContext("2d");
+
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
+  lienzo.beginPath();
+  lienzo.rect(xinicial, yinicial, xfinal, yfinal);
+  lienzo.fillStyle = color;
+  lienzo.fill();
+}
+
+function dibujarRectangulo (Base, Altura) {
+  var xi = (ancho / 2) - (Base / 2);
+  var yi = (alto / 2) - (Altura / 2);
+  var xf = Base;
+  var yf = Altura;
+  dibujarLinea("red", xi, yi, xf, yf);
+}*/
+
+
+
+/*******************************************************************************
+
+    BORRAR DATOS, RESULTADO Y DIBUJO
+    
+*******************************************************************************/
+var boton_borrar = document.getElementById("botonBorrar");
+boton_borrar.addEventListener("click", borraDatos);
+
+function borraDatos ()
+{
+  //Borra los resultados
+  document.getElementById('resultadoArea').innerHTML="";
+  document.getElementById('resultadoPerimetro').innerHTML="";
+  //Borra el dibujo en el canvas
+  lienzo.clearRect(0, 0, d.width, d.height);
+}
